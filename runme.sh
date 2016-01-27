6cba0ed5b5916f83423f64d364329ac57ee00341
Jordi Prats <jprats@systemadmin.es>
2016-01-27 12:51:24 +0100

    permetre allow from any

--- runme.sh
+++ runme.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sed -e 's@^Allow.*@Allow '${EYP_ALLOW}'@g' -i /etc/tinyproxy.conf
+if [ -z "${EYP_ALLOW}" ];
+then
+  sed -e 's@^Allow.*@@g' -i /etc/tinyproxy.conf
+else
+  sed -e 's@^Allow.*@Allow '${EYP_ALLOW}'@g' -i /etc/tinyproxy.conf
+fi
+
 
 exec /usr/sbin/tinyproxy -d
